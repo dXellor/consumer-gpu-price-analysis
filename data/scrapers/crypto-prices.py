0fc60ef15da6fd40a5f1a1208c4793a68c8b7b6a
N. Simic <nikola7simic@gmail.com>
2025-08-08 22:51:22 +0200

    feat: aquire historical data of cryptocurrency prices

---
+++ data/scrapers/crypto-prices.py
@@ -0,0 +1,3 @@
+# Sucesfully downloaded from https://coincodex.com, no scraping needed
+# URL: https://coincodex.com/crypto/ethereum-classic/historical-data/, https://coincodex.com/crypto/monero/historical-data/
+# Period: 01.08.2023 - 01.08.2025.
